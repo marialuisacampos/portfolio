0e2e0a5e889bbeb38ea301687776826765749d63
marialuisacampos <luisa.campos@ufrpe.br>
2022-09-02 17:03:29 -0300

    Change ' at test for &apos;

--- maluportfolio/src/components/About.js
+++ maluportfolio/src/components/About.js
@@ -9,8 +9,8 @@ const About = () => {
         <Photo />
       </div>
       <p className='mt-4 text-gray-600 text-sm md:text-base md:text-left md:mt-2 md:mr-28'>Junior Frontend Developer majoring in Information Systems at the UFRPE. I see myself as a curious, with an ease of learning, communicative and organized professional. I love learning and developing inovative systems.</p>
-      <p className='mt-4 text-gray-600 text-sm md:text-base md:text-left md:mt-2 md:mr-28 italic'>My studies and experiences are focused on the JavaScript stack, but at the end of the day I'm a developer who accepts and loves new chalenges.</p>
-      <p className='mt-4 text-gray-600 text-sm md:text-base md:text-left md:mt-2 md:mr-28'>During my free times I'm an aerial acrobat, artisan and voracious book reader. Also a great admirer of genius horror movies and cat lover!</p>
+      <p className='mt-4 text-gray-600 text-sm md:text-base md:text-left md:mt-2 md:mr-28 italic'>My studies and experiences are focused on the JavaScript stack, but at the end of the day I&apos;m a developer who accepts and loves new chalenges.</p>
+      <p className='mt-4 text-gray-600 text-sm md:text-base md:text-left md:mt-2 md:mr-28'>During my free times I&apos;m an aerial acrobat, artisan and voracious book reader. Also a great admirer of genius horror movies and cat lover!</p>
       <p className='mt-4 text-gray-600 text-sm md:text-base md:text-left md:mt-2 md:mr-28'>Oh, and the biggest fan of Coldplay you will ever met :)</p>
     </section>
   )
--- maluportfolio/src/components/Profile.js
+++ maluportfolio/src/components/Profile.js
@@ -4,10 +4,10 @@ import { HiDownload } from "react-icons/hi";
 const Header = () => {
   return (
     <div className='flex flex-col gap-4 ml-20 md:ml-56 pr-10 md:pr-8'>
-      <h1 className='font-bold text-4xl md:text-6xl'>Hi! I'm Maria Luísa</h1>
+      <h1 className='font-bold text-4xl md:text-6xl'>Hi! I&apos;m Maria Luísa</h1>
       <span className='text-sm md:text-base'>FrontEnd Developer</span>
-      <p className='text-gray-400 text-sm md:text-base'>Based in Recife, Brazil. Motivated by improving inclusion and diversity and impacting people's lives through technology.</p>
-      <a className='text-sm md:text-base bg-black max-w-[130px] text-white-100 flex justify-center gap-2 rounded-lg py-2 md:py-4' href="/">Resume <HiDownload className='self-center' /></a>
+      <p className='text-gray-400 text-sm md:text-base'>Based in Recife, Brazil. Motivated by improving inclusion and diversity and impacting people&apos;s lives through technology.</p>
+      <a className='text-sm md:text-base bg-black max-w-[130px] text-white-100 flex justify-center gap-2 rounded-lg py-2 md:py-4' href="https://linkedin.com/in/marialuisacampos">Resume <HiDownload className='self-center' /></a>
     </ div>
   )
 }
